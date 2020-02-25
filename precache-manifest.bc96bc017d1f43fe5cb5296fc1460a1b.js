5e68c41ba1b3419ea2e68de7a58ef7ced16fd579
KartachovIA <ikartachov@yandex.ru>
2020-02-25 23:04:18 +0300

    Updates

---
+++ precache-manifest.bc96bc017d1f43fe5cb5296fc1460a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25af375bb66e7ce02eeb555b8900258",
+    "url": "/my-first-presentation/index.html"
+  },
+  {
+    "revision": "7ecd7fe577adedc11da7",
+    "url": "/my-first-presentation/static/css/main.055bdc09.chunk.css"
+  },
+  {
+    "revision": "a90de39a5a51cc8a9483",
+    "url": "/my-first-presentation/static/js/2.d9d0e9af.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-first-presentation/static/js/2.d9d0e9af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ecd7fe577adedc11da7",
+    "url": "/my-first-presentation/static/js/main.612d07b8.chunk.js"
+  },
+  {
+    "revision": "4cb3f8700484829116b7",
+    "url": "/my-first-presentation/static/js/runtime-main.482bb820.js"
+  }
+]);
